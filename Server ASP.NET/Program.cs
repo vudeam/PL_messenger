ff5ce6e797695479ad78ff353c27e1b92311a5c8
Vadim <vadim22.barnaul@gmail.com>
2020-11-08 18:24:08 +0300

    Make server demo with 2 simple requests

--- Server ASP.NET/Program.cs
+++ Server ASP.NET/Program.cs
@@ -1,26 +1,53 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Server_ASP.NET
 {
-	public class Program
+	public struct Message
 	{
-		public static void Main(string[] args)
+		public String Content { get; set; }
+		public String FromID { get; set; }
+		public DateTime Timestamp { get; set; }
+
+		public override String ToString()
+		{
+			return String.Format(
+				"[{0}] {1} : {2}",
+				this.Timestamp.ToLongTimeString(),
+				this.FromID,
+				this.Content
+			);
+		}
+	}
+
+	public class Server
+	{
+		public static void Main(String[] args)
 		{
 			CreateHostBuilder(args).Build().Run();
+			Console.WriteLine("\nPress Enter to close this window...");
+			Console.ReadLine();
+
+			return;
 		}
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
+		public static IHostBuilder CreateHostBuilder(String[] args) =>
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
-					webBuilder.UseStartup<Startup>();
+					webBuilder
+						.UseKestrel()
+						.UseContentRoot(Directory.GetCurrentDirectory())
+						.UseUrls(
+							"http://0.0.0.0:5005"
+						)
+						.UseStartup<Startup>();
 				});
 	}
 }
