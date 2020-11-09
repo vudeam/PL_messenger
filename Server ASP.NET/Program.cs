c799f211759ed6e4d4ea1fac7c79d94f090686c6
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2020-11-09 14:17:25 +0300

    Revert "Make server demo with 2 simple requests"
    This reverts commit ff5ce6e797695479ad78ff353c27e1b92311a5c8.

--- Server ASP.NET/Program.cs
+++ Server ASP.NET/Program.cs
@@ -1,53 +1,26 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Server_ASP.NET
 {
-	public struct Message
+	public class Program
 	{
-		public String Content { get; set; }
-		public String FromID { get; set; }
-		public DateTime Timestamp { get; set; }
-
-		public override String ToString()
-		{
-			return String.Format(
-				"[{0}] {1} : {2}",
-				this.Timestamp.ToLongTimeString(),
-				this.FromID,
-				this.Content
-			);
-		}
-	}
-
-	public class Server
-	{
-		public static void Main(String[] args)
+		public static void Main(string[] args)
 		{
 			CreateHostBuilder(args).Build().Run();
-			Console.WriteLine("\nPress Enter to close this window...");
-			Console.ReadLine();
-
-			return;
 		}
 
-		public static IHostBuilder CreateHostBuilder(String[] args) =>
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
-					webBuilder
-						.UseKestrel()
-						.UseContentRoot(Directory.GetCurrentDirectory())
-						.UseUrls(
-							"http://0.0.0.0:5005"
-						)
-						.UseStartup<Startup>();
+					webBuilder.UseStartup<Startup>();
 				});
 	}
 }
--- Server ASP.NET/Startup.cs
+++ Server ASP.NET/Startup.cs
@@ -1,75 +1,40 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Server_ASP.NET
 {
 	public class Startup
 	{
-		public IConfiguration Configuration { get; }
-
-		public Startup(IConfiguration _Configuration)
-		{
-			this.Configuration = _Configuration;
-		}
-
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllers();
-
-			return;
 		}
 
-		/// <summary>
-		/// Set:
-		/// <list type="bullet">
-		/// <item>Usage of development environment</item>
-		/// <item>Method called on application stop</item>
-		/// <item>Controllers mapping</item>
-		/// <item>Handler for default (or incorrect) route request</item>
-		/// </list>
-		/// </summary>
-		public void Configure(
-			IApplicationBuilder      app,
-			IWebHostEnvironment      env,
-			IHostApplicationLifetime lifetime,
-			ILogger<Startup>         logger
-		)
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
 
-			lifetime.ApplicationStopping.Register(() => Console.WriteLine("Graceful stop"));
-
 			app.UseRouting();
 
 			app.UseEndpoints(endpoints =>
 			{
-				//endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-				endpoints.MapControllers();
+				endpoints.MapGet("/", async context =>
+				{
+					await context.Response.WriteAsync("Hello World!");
+				});
 			});
-
-			app.Run(async (context) =>
-			{
-				context.Response.StatusCode = StatusCodes.Status404NotFound;
-				await context.Response.WriteAsync("<h1>404 Not Found</h1>");
-				logger.Log(
-					LogLevel.Information,
-					"{0,6} {1} {2}",
-					context.Request.Method,
-					context.Response.StatusCode,
-					context.Request.Path
-				);
-			});
-
-			return;
 		}
 	}
 }
