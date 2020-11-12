b14c2a6f7169707083380a3a35c1c412a43d15e7
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2020-11-12 23:05:42 +0300

    Add config struct

---
+++ Utilities/config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VectorChat.Utilities
+{
+	public struct config
+	{
+		public uint messageRequestTime { get; set; }
+		public string ServerAddress { get; set; }
+		public config(uint _messageRequestTime, string _ServerAdress) {
+			messageRequestTime = _messageRequestTime;
+			ServerAddress = _ServerAdress;
+		}
+	}
+}
