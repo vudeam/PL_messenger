78abf9602a92242e5a425481a0f5d2f8945e5fdc
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2020-11-14 02:31:54 +0300

    Add product name to namespace

--- Client_WPF/App.xaml.cs
+++ Client_WPF/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Client_WPF
+namespace VectorChat.Client_WPF
 {
     /// <summary>
     /// Interaction logic for App.xaml
