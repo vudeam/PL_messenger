54118b43fe356ef2fa629dc24687ee2ee5bcb63c
Vadim Ermakov <vadim22.barnaul@gmail.com>
2020-11-16 00:13:48 +0300

    Rename ChatEngine -> ChatController

--- Server_ASPNET/Controllers/ChatController.cs
+++ Server_ASPNET/Controllers/ChatController.cs
@@ -6,30 +6,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
 using VectorChat.Utilities;
 
 namespace VectorChat.ServerASPNET.Controllers
 {
 	/// <summary>
-	/// Route: /api
+	/// Route: /api/chat
 	/// </summary>
-	[Route("api")]
+	[Route("api/[controller]")]
 	[ApiController]
-	public class ChatEngine : ControllerBase
+	public class ChatController : Controller
 	{
 		public static readonly ILogger consoleLogger = LoggerFactory.Create(logBuilder =>
 		{
 			logBuilder.AddConsole();
 			logBuilder.AddDebug();
-		}).CreateLogger<ChatEngine>();
+		}).CreateLogger<ChatController>();
 
-		//public static readonly ILogger fileLogger = new FileLogger(Path.Combine(Directory.GetCurrentDirectory(), "log.txt"));
+		//public static readonly ILogger fileLogger = new FileLogger(Path.Combine(Directory.GetCurrentDirectory(), "ChatLog.txt"));
 
 		public static List<Message> messages = new List<Message>();
 
 		/// <summary>
-		/// Route: GET /api/messages
+		/// GET messages
 		/// </summary>
 		[HttpGet("messages")]
 		[Produces("application/json")]
@@ -43,9 +42,7 @@ namespace VectorChat.ServerASPNET.Controllers
 			//	this.Request.Path,
 			//	messages.Count
 			//);
-			consoleLogger.Log(
-				LogLevel.Information,
-				"{0,6} {1} {2}, output {3} message(-s)",
+			consoleLogger.Log(LogLevel.Information, "{0,6} {1} {2}, output {3} message(-s)",
 				this.Request.Method,
 				this.Response.StatusCode,
 				this.Request.Path,
@@ -56,7 +53,7 @@ namespace VectorChat.ServerASPNET.Controllers
 		}
 
 		/// <summary>
-		/// POST /api/messages
+		/// POST messages
 		/// </summary>
 		[HttpPost("messages")]
 		public IActionResult PostMessage([FromBody] Message IncomingMessage)
@@ -71,9 +68,7 @@ namespace VectorChat.ServerASPNET.Controllers
 			//	this.Request.Path,
 			//	messages[messages.Count - 1].ToString()
 			//);
-			consoleLogger.Log(
-				LogLevel.Information,
-				"{0,6} {1} {2}, got Message: {3}",
+			consoleLogger.Log(LogLevel.Information, "{0,6} {1} {2}, got Message: {3}",
 				this.Request.Method,
 				this.Response.StatusCode,
 				this.Request.Path,
@@ -84,7 +79,7 @@ namespace VectorChat.ServerASPNET.Controllers
 		}
 
 		/// <summary>
-		/// Route: GET /api/welcome
+		/// GET welcome
 		/// </summary>
 		[HttpGet("welcome")]
 		public IActionResult WelcomeMessage()
@@ -110,7 +105,7 @@ namespace VectorChat.ServerASPNET.Controllers
 		}
 
 		/// <summary>
-		/// Route: GET /api/welcome/{_name}
+		/// GET welcome/{_name}
 		/// </summary>
 		[HttpGet("welcome/{_name}")]
 		[Produces("application/json")]
@@ -137,8 +132,36 @@ namespace VectorChat.ServerASPNET.Controllers
 		}
 
 		/// <summary>
-		/// Route: POST /api/reauestBody
+		/// GET welcome/{_name}
 		/// </summary>
+		[HttpGet("welcome/{_name}/{_referer}")]
+		[Produces("application/json")]
+		public IActionResult WelcomeReferer(String _name, String _referer)
+		{
+			//fileLogger.Log(
+			//	LogLevel.Information,
+			//	"{0,6} {1} {2}",
+			//	this.Request.Method,
+			//	this.Response.StatusCode,
+			//	this.Request.Path
+			//);
+			consoleLogger.Log(
+				LogLevel.Information,
+				"{0,6} {1} {2}",
+				this.Request.Method,
+				this.Response.StatusCode,
+				this.Request.Path
+			);
+
+			messages.Add(new Message() { Content = $"Hello, {_name} from {_referer}!", FromID = "id0", Timestamp = DateTime.Now });
+
+			return Ok(JsonSerializer.Serialize($"Welcome, {_name} from {_referer}!"));
+		}
+
+		/// <summary>
+		/// POST reauestBody
+		/// </summary>
+		/// <param name="value">Request body (of type <c>int</c>), serialized in JSON</param>
 		[HttpPost("requestBody")]
 		public IActionResult OutputRequest([FromBody] int value)
 		{
@@ -150,9 +173,7 @@ namespace VectorChat.ServerASPNET.Controllers
 			//	this.Request.Path,
 			//	value.ToString()
 			//);
-			consoleLogger.Log(
-				LogLevel.Information,
-				"{0,6} {1} {2} :{3}",
+			consoleLogger.Log(LogLevel.Information, "{0,6} {1} {2} :{3}",
 				this.Request.Method,
 				this.Response.StatusCode,
 				this.Request.Path,
