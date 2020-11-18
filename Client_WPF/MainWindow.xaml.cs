3488aa43415b95a0a71096ec27afc20f713f7329
Иван Агабалаев <61901199+Betaron@users.noreply.github.com>
2020-11-18 16:07:56 +0300

    Add message output to the ScrollList as auto size bubbles

--- Client_WPF/MainWindow.xaml.cs
+++ Client_WPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -31,7 +32,6 @@ namespace VectorChat.Client_WPF
 		public MainWindow()
 		{
 			InitializeComponent();
-			
 		}
 
 		/// <summary>
@@ -39,7 +39,7 @@ namespace VectorChat.Client_WPF
 		/// Sets the area and size of the textbox based on its content.
 		/// </summary>
 		private void ReloadTextBoxHeight(TextBox textBox, RowDefinition gridCellHeight, double gridCellStartHeight, int maxLines)
-        {
+		{
 			if (textBox.LineCount < maxLines && IsLoaded)
 			{
 				gridCellHeight.Height = new GridLength(gridCellStartHeight + (textBox.LineCount - 1) * (textBox.FontSize + 4));
@@ -61,6 +61,7 @@ namespace VectorChat.Client_WPF
 			
 			//Getting start information
 			messageTextBoxCellStartHeight = messageTextBoxCellHeight.Height.Value;
+			messagesRequesing();
 		}
 
 		private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -108,24 +109,114 @@ namespace VectorChat.Client_WPF
 				messageTextBox.Text = "";
 			}
 		}
-
+ 
 		private void messagesRequesing()
 		{
-			HttpWebRequest mesFromServer = (HttpWebRequest)WebRequest.Create(configInfo.serverAddress + "/api/messages");
-			mesFromServer.Method = "GET";
-			mesFromServer.ContentType = "Application/json";
+			Message message1 = new Message()
+			{
+				Content = "Test message 1",
+				FromID = "TestUser #1",
+				Timestamp = DateTime.Now
+			};
+			Message message2 = new Message()
+			{
+				Content = "Test message 2",
+				FromID = "TestUser #2",
+				Timestamp = DateTime.Now
+			};
+
+			BuildMessageBubble(messagesList, message1);
+			BuildMessageBubble(messagesList, message2);
 		}
 
-        private void messageTextBox_GotFocus(object sender, RoutedEventArgs e)
-        {
-			if (messageTextBox.Text == "")
-				enterSign.Opacity = 0;
-        }
+		/// <summary>
+		///	Adds a message unit to the specified ListBox.
+		/// </summary>
+		/// <param name="_msg"></param>
+		/// <param name="messagesArea"></param>
+		private void BuildMessageBubble(ListBox messagesArea, Message _msg)
+		{
+			var mainGrid = new Grid();
+			var messageGrid = new Grid();
+			var vertStack = new StackPanel();
+			var horizStack = new StackPanel();
 
-        private void messageTextBox_LostFocus(object sender, RoutedEventArgs e)
-        {
-			if (messageTextBox.Text == "")
+			vertStack.Orientation = Orientation.Vertical;
+			horizStack.Orientation = Orientation.Horizontal;
+
+			var nickname = new Label()
+			{
+				Content = _msg.FromID,
+				FontSize = 14,
+				Background = null,
+				BorderBrush = null
+			};
+			vertStack.Children.Add(nickname);
+
+
+			var rect = new Rectangle()
+			{
+				Fill = new SolidColorBrush(Color.FromRgb(180, 255, 200)),
+				RadiusX = 10,
+				RadiusY = 10,
+				Focusable = false
+			};
+			messageGrid.Children.Add(rect);
+
+			var tb = new TextBox()
+			{
+				TextWrapping = TextWrapping.Wrap,
+				Width = 240	,
+				Text = _msg.Content,
+				FontSize = 16,
+				Background = null,
+				BorderBrush = null,
+				Focusable = false
+			};
+			messageGrid.Children.Add(tb);
+			horizStack.Children.Add(messageGrid);
+
+			var time = new Label()
+			{
+				Content = _msg.Timestamp.ToShortTimeString(),
+				FontSize = 14,
+				VerticalAlignment = VerticalAlignment.Bottom,
+				Margin = new Thickness(0, 0, 5, 0),
+				Foreground = Brushes.DarkGray,
+				Background = null,
+				BorderBrush = null
+			};
+			horizStack.Children.Add(time);
+			vertStack.Children.Add(horizStack);
+			mainGrid.Children.Add(vertStack);
+
+			messagesArea.Items.Add(mainGrid);
+			var renderedMessageGrid = (((messagesArea.Items[messagesArea.Items.Count-1] as Grid).Children[0] as StackPanel).Children[1] as StackPanel).Children[0] as Grid;
+			foreach (var objects in renderedMessageGrid.Children)
+			{
+				if (objects.GetType().Equals(typeof(TextBox)))
+				{
+					TextBox renderedTb = objects as TextBox;
+					renderedTb.UpdateLayout();
+					renderedTb.Width = tb.ExtentWidth + 6;
+					renderedTb.Height = tb.ExtentHeight + 2;
+					renderedMessageGrid.Width = tb.Width + 14;
+					renderedMessageGrid.Height = tb.Height + 10;
+				}
+			}
+			(messagesArea.Items[messagesArea.Items.Count - 1] as Grid).Margin = new Thickness(15, 10, 100, 5);
+		}
+
+		private void messageTextBox_GotFocus(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrEmpty(messageTextBox.Text))
+				enterSign.Opacity = 0;
+		}
+
+		private void messageTextBox_LostFocus(object sender, RoutedEventArgs e)
+		{
+			if (string.IsNullOrEmpty(messageTextBox.Text))
 				enterSign.Opacity = 1;
-        }
-    }
+		}
+	}
 }
