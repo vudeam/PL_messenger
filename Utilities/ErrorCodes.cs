497114ad3e4b7d0f89fb758e136c478ac08c13e8
Vadim Ermakov <vadim22.barnaul@gmail.com>
2020-11-20 00:03:15 +0300

    Add XML comments for ApiErrCodes

--- Utilities/ErrorCodes.cs
+++ Utilities/ErrorCodes.cs
@@ -2,9 +2,37 @@
 {
 	public enum ApiErrCodes
 	{
+		/// <summary>
+		/// Code for successful operation.<br/>
+		/// The default message for this code is "OK"
+		/// </summary>
 		Success = 0,
+
+		/// <summary>
+		/// <see cref="VectorChat.Utilities.Credentials.Account"/> with the same login already exists. 
+		/// Registration is impossible.<br/>
+		/// The default message for this code is "Registered account with the same login already exists."
+		/// </summary>
 		LoginTaken,
+
+		/// <summary>
+		/// <see cref="VectorChat.Utilities.Credentials.Account"/> with the provided login is not present among registered accounts. 
+		/// Authentication is impossible.<br/>
+		/// The default message for this code is "Account with this login does not exist."
+		/// </summary>
+		LoginNotFound,
+
+		/// <summary>
+		/// Password for the provided <see cref="VectorChat.Utilities.Credentials.Account"/> does not match with the registered one. 
+		/// Authentication failed.<br/>
+		/// The default message for this code is "Incorrect password."
+		/// </summary>
 		PasswordIncorrect,
+
+		/// <summary>
+		/// An unknown error occured which can not be described by existing error codes<br/>
+		/// The default message for this code is "Unknown error."
+		/// </summary>
 		Unknown
 	}
 }
