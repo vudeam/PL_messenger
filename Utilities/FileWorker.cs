d9690addf5ff0093fde55a70d78ddd76a99d3cdb
Vadim Ermakov <vadim22.barnaul@gmail.com>
2020-12-01 01:17:36 +0300

    Add Newtonsoft.Json

--- Utilities/FileWorker.cs
+++ Utilities/FileWorker.cs
@@ -1,18 +1,45 @@
 ﻿using System.IO;
-using System.Text.Json;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
 
 namespace VectorChat.Utilities
 {
 	public static class FileWorker
 	{
-		public static void SaveToFile<TItem>(string path, TItem item, JsonSerializerOptions options = null)
+		public static void SaveToFile(string path, object item, Formatting options = Formatting.Indented)
 		{
-			File.WriteAllText(path, JsonSerializer.Serialize(item, item.GetType(), options));
+			File.WriteAllText(path, JsonConvert.SerializeObject(item, options));
 		}
 
-		public static TItem LoadFromFile<TItem>(string path, JsonSerializerOptions options = null)
+		public static TItem LoadFromFile<TItem>(string path, JsonSerializerSettings settings = null)
 		{
-			return JsonSerializer.Deserialize<TItem>(File.ReadAllText(path), options);
+			return JsonConvert.DeserializeObject<TItem>(File.ReadAllText(path), settings);
+		}
+
+		public static async void SaveToFileAsync(string path, object item, Formatting options = Formatting.Indented)
+		{
+			System.Console.WriteLine("Writing to file async...");
+			await Task.Run(() => File.WriteAllTextAsync(path, JsonConvert.SerializeObject(item, options)));
+			System.Console.WriteLine("Finished writing to file");
+		}
+
+		public static void SaveToBinary(string path, object item, FileMode mode = FileMode.Append)
+		{
+			BinaryFormatter formatter = new BinaryFormatter();
+			using (FileStream fstream = new FileStream(path, mode))
+			{
+				formatter.Serialize(fstream, item);
+			}
+		}
+
+		public static TItem LoadFromBinary<TItem>(string path, FileMode mode = FileMode.Open)
+		{
+			BinaryFormatter formatter = new BinaryFormatter();
+			using (FileStream fstream = new FileStream(path, mode))
+			{
+				return (TItem)formatter.Deserialize(fstream);
+			}
 		}
 	}
 }
